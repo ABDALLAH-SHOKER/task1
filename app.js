4232bcac18e1cd425d3344e3fae28ff2ab26d0fa
ABDALLAH-SHOKER <74017781+ABDALLAH-SHOKER@users.noreply.github.com>
2021-11-14 14:37:52 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express()
+
+app.get('/',(req,res)=>res.send('hello world '));
+
+app.listen (3001,() =>{
+    console.log('my first API running on port 3001');
+})
